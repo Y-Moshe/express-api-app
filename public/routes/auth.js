fbdda2a1b7b99d4634a11e173489a3a15082c12e
y-moshe <moshe.nehemiah@gmail.com>
2021-07-02 18:52:26 +0300

    auth routes

---
+++ public/routes/auth.js
@@ -0,0 +1,9 @@
+const routes = require('express').Router();
+
+const controllers = require( '../controllers/auth' );
+
+routes.post( '/signup', controllers.createUser );
+
+routes.post( '/login', controllers.loginUser );
+
+module.exports = routes;
