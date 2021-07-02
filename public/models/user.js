154ae612ffa24993c0c77fed5d6b62e3f92ba088
y-moshe <moshe.nehemiah@gmail.com>
2021-07-02 18:50:07 +0300

    models

--- public/models/user.js
+++ public/models/user.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    unique: true
+  }
 });
 
-module.exports = mongoose.model('User', schema);
+module.exports = mongoose.model( 'User' , userSchema );
